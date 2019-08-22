e9780cde3748ee393a13fb479f3b7688b6ec3912
xiexinfang <1036554353@qq.com>
2019-08-22 21:14:39 +0800

    05

--- src/main/java/practice05/Student.java
+++ src/main/java/practice05/Student.java
@@ -1,4 +1,26 @@
 package practice05;
 
-public class Student {
+import practice05.Person;
+
+public class Student extends Person{
+	private int klass;
+
+	public Student(String name, int age, int klass) {
+		super(name, age);
+		this.klass = klass;
+	}
+
+	public int getKlass() {
+		return klass;
+	}
+
+	public void setKlass(int klass) {
+		this.klass = klass;
+	}
+
+	@Override
+	public String introduce() {
+		return String.format("My name is %s. I am %d years old. I am a Student. I am at Class %d.", this.getName(), this.getAge(), this.getKlass());
+	}
 }
+ 
--- src/main/java/practice05/Worker.java
+++ src/main/java/practice05/Worker.java
@@ -1,4 +1,16 @@
 package practice05;
 
-public class Worker {
+import practice05.Person;
+
+public class Worker extends Person{
+
+	public Worker(String name, int age) {
+		super(name, age);
+	}
+
+	@Override
+	public String introduce() {
+		return String.format("My name is %s. I am %d years old. I am a Worker. I have a job.", this.getName(), this.getAge());
+	}
 }
+ 
