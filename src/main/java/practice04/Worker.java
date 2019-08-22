fd06f9188ad671ae267573035c00f6bba8e68909
xiexinfang <1036554353@qq.com>
2019-08-22 21:10:52 +0800

    03

--- src/main/java/practice04/Worker.java
+++ src/main/java/practice04/Worker.java
@@ -1,4 +1,14 @@
 package practice04;
 
-public class Worker {
+public class Worker extends Person{
+
+	public Worker(String name, int age) {
+		super(name, age);
+	}
+
+	@Override
+	public String introduce() {
+		return String.format("My name is %s. I am %d years old. I am a Worker. I have a job.", this.getName(), this.getAge());
+	}
 }
+ 
