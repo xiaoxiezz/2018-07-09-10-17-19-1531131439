83fd47c8a2f25ec0f99959d0a433e1b4bf92476c
xiexinfang <1036554353@qq.com>
2019-08-23 09:51:48 +0800

    Kalss has getNumber

--- src/main/java/practice07/Klass.java
+++ src/main/java/practice07/Klass.java
@@ -1,4 +1,13 @@
 package practice07;
 
 public class Klass {
+	private int klass;
+	
+	public Klass(int klass){
+		this.klass=klass;
+	}
+	
+	public int getNumber(){
+		return klass;
+	}
 }
--- src/test/java/practice07/Practice07Test.java
+++ src/test/java/practice07/Practice07Test.java
@@ -8,10 +8,10 @@ import static org.fest.assertions.api.Assertions.assertThat;
 public class Practice07Test {
     private Klass klass;
 //
-//    @Before
-//    public void setup() {
-//        klass = new Klass(2);
-//    }
+    @Before
+    public void setup() {
+        klass = new Klass(2);
+    }
 //
     @Test
     public void should_person_have_name_and_age() throws Exception {
@@ -27,10 +27,10 @@ public class Practice07Test {
         assertThat(introduce).isEqualTo("My name is Tom. I am 21 years old.");
     }
 //
-//    @Test
-//    public void should_class_have_a_number() throws Exception {
-//        assertThat(klass.getNumber()).isEqualTo(2);
-//    }
+    @Test
+    public void should_class_have_a_number() throws Exception {
+        assertThat(klass.getNumber()).isEqualTo(2);
+    }
 //
 //    @Test
 //    public void should_class_get_display_name() throws Exception {
